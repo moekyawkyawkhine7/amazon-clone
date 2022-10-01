a9c1512a2d0685c6553a5daf7cdb1113c2ae2f6d
Moe Kyaw Kyaw Khine <88232653+moekyawkyawkhine7@users.noreply.github.com>
2022-10-01 21:38:45 +0630

    Day 4 90% done

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,15 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
-    apiKey: "AIzaSyAnOcozrfvjOIaSCDwTyQHj8ucWBTsKnjg",
     authDomain: "amz-clone-cc9a2.firebaseapp.com",
+    apiKey: "AIzaSyAnOcozrfvjOIaSCDwTyQHj8ucWBTsKnjg",
     projectId: "amz-clone-cc9a2",
     storageBucket: "amz-clone-cc9a2.appspot.com",
     messagingSenderId: "1007980430060",
     appId: "1:1007980430060:web:136bd51f8514f0041fdbc9"
 };
+
+const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
+const db = app.firestore();
+
+export default db;
--- src/pages/orders.js
+++ src/pages/orders.js
@@ -1,15 +1,65 @@
+import moment from 'moment/moment';
+import { getSession } from 'next-auth/react';
 import React from 'react'
 import Header from "../components/Header";
+import db from '../firebase';
 
-const orders = () => {
+const orders = ({
+    orders
+}) => {
+
+    console.log("orders:", orders)
+
     return (
         <div className="h-screen bg-gray-100">
             <Header />
             <main className="breakout-point">
-
+                <div className="w-full md:w-6/12 py-2 mx-auto">
+                    <p className="text-2xl font-semibold pb-1 border-b-2 border-yellow-500">Your Orders</p>
+                    <div className="flex">
+                        Item
+                    </div>
+                </div>
             </main >
         </div >
     )
 }
 
 export default orders
+
+export async function getServerSideProps(context) {
+    const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+    const session = await getSession(context); // for server
+    if (!session) {
+        return {
+            props: {}
+        }
+    }
+    let { user: { email } } = session;
+
+    // From Firebase
+    const ordersFromDB = await db
+        .collection("users")
+        .doc(email)
+        .collection("orders")
+        .orderBy("timestamp", "desc")
+        .get();
+
+    // From Stripe
+    const orders = await Promise.all(ordersFromDB.docs.map(async _order => ({ // multiple promises to one promise, process by process
+        id: _order.id,
+        amount: _order.data().amount,
+        amountShipping: _order.data().amount_shipping,
+        images: _order.data().images,
+        timestamp: moment(_order.data().timestamp.toDate()).unix(), // unix(): to get the number of second
+        items: (await stripe.checkout.sessions.listLineItems(_order.id, {
+            limit: 100
+        })).data
+    })))
+
+    return {
+        props: {
+            orders
+        }, // will be passed to the page component as props
+    }
+}
